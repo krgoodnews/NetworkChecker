e778f91ed0502d7a0635378202f334d0456557cd
Yunsu Guk <krgoodnews@gmail.com>
2019-08-18 00:13:30 +0900

    Create Project

---
+++ NetworkChecker/NetworkChecker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NetworkChecker
+//
+//  Created by 국윤수 on 18/08/2019.
+//  Copyright © 2019 국윤수. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ NetworkChecker/NetworkChecker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NetworkChecker
+//
+//  Created by 국윤수 on 18/08/2019.
+//  Copyright © 2019 국윤수. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
